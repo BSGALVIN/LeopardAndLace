ab462a04b504d3291cce2078a477c9c1c63f9b6b
elizajasp <elizagjasp@gmail.com>
2023-05-26 10:51:05 +1000

    trying shopping cart

---
+++ shoopingcart/shoppingcart.js
@@ -0,0 +1,7 @@
+document.getElementById("cartButton").addEventListener("click", function() {
+    document.getElementById("cartPopup").style.display = "block";
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+    document.getElementById("cartPopup").style.display = "none";
+});
