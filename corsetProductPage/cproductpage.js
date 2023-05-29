72f6cfb3904ac6f16d1f0e938dc834e00f7097b4
BSGALVIN <biancagalvin@gmail.com>
2023-05-29 15:08:53 +1000

    corsetpage looks oh so much better

--- corsetProductPage/cproductpage.js
+++ corsetProductPage/cproductpage.js
@@ -1,3 +1,19 @@
+//image switch
+$(document).ready(function() {
+ 
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+ 
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+ 
+});
+
+
+
+// quantity
 function increaseValue() {
     var value = parseInt(document.getElementById('number').value, 10);
     value = isNaN(value) ? 0 : value;
