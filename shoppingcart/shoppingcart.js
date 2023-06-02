c5ae671755021c928c371a1d9cb80b91ab66c97e
elizajasp <elizagjasp@gmail.com>
2023-06-02 16:35:02 +1000

    cart pop up javascript

--- shoppingcart/shoppingcart.js
+++ shoppingcart/shoppingcart.js
@@ -1,7 +1,8 @@
-document.getElementById("cartButton").addEventListener("click", function() {
-    document.getElementById("cartPopup").style.display = "block";
-});
+// document.getElementById("cartButton").addEventListener("click", function() {
+//     document.getElementById("cartPopup").style.display = "block";
+// });
 
-document.getElementById("closeButton").addEventListener("click", function() {
-    document.getElementById("cartPopup").style.display = "none";
-});
+// document.getElementById("closeButton").addEventListener("click", function() {
+//     document.getElementById("cartPopup").style.display = "none";
+// });
+
