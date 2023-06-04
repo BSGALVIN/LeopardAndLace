852e1e8f8c172fef326d3a339bdf552d97196720
BSGALVIN <biancagalvin@gmail.com>
2023-06-04 15:07:30 +1000

    header and border sizing

--- homePage/homepage.js
+++ homePage/homepage.js
@@ -5,3 +5,11 @@ document.getElementById("cartButton").addEventListener("click", function() {
 document.getElementById("closeButton").addEventListener("click", function() {
     document.getElementById("cartPopup").style.display = "none";
 });
+
+document.getElementById("cart-btn").addEventListener("click", function() {
+    document.getElementById("cartPopup").style.display = "block";
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+    document.getElementById("cartPopup").style.display = "none";
+});
